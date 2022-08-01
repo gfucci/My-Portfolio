1976cd7c73fc690060ebf90cb91fc0dcaf56d40c
Gabriel Fucci <gfucci_2005@hotmail.com>
2022-08-01 01:12:54 -0300

    Termina CSS das páginas principais

--- src/components/text/Text.jsx
+++ src/components/text/Text.jsx
@@ -13,14 +13,14 @@ const Text = (
     }) => {
 
   return (
-    <div>
+    <div className='text_container'>
         <h3>{title}</h3>
-        {subtitle && <span>{subtitle}</span>}
-        {year && <span> | {year}</span>}
-        {study && <span> | {study}</span> }
-        {graduate && <span> | {graduate}</span> }
-        {paragraph && <p>{paragraph}</p>}
-        {links && <a href={links} target="_blank" rel="noreferrer noopener">{links}</a>}
+          {subtitle && <span>{subtitle}</span>}
+          {year && <span> | {year}</span>}
+          {study && <span> | {study}</span> }
+          {graduate && <span> | {graduate}</span> }
+          {paragraph && <p>{paragraph}</p>}
+          {links && <a href={links} target="_blank" rel="noreferrer noopener">{links}</a>}
     </div>
   )
 }
