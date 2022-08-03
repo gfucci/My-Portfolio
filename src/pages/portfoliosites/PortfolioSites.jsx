a4e7214a7cfd47c6727fc4912cec3af65dde1214
Gabriel Fucci <gfucci_2005@hotmail.com>
2022-08-03 00:52:46 -0300

    Termina boxes dos sites estáticos

--- src/pages/portfoliosites/PortfolioSites.jsx
+++ src/pages/portfoliosites/PortfolioSites.jsx
@@ -13,52 +13,52 @@ const PortfolioSites = () => {
       <Title title="Portfólio Sites Estáticos" icon={<FaHtml5 />} />
       <div className="boxes_container">
         <Box
-          title="React Blog"
-          subtitle="React.js | Firebase"
-          preview="Blog feito em react e firebase."
+          title="Clone Whats App Web"
+          subtitle="HTML5 | CSS3"
+          preview="Um clone estático da home page do Wpp WEB."
           id={uuidv4()}
           className='test'
         />
         <Box
-          title="React Blog"
-          subtitle="React.js | Firebase"
-          preview="Blog feito em react e firebase."
+          title="Clone Instagram Home"
+          subtitle="HTML5 | CSS3 | Responsividade"
+          preview="Clone do intagram com responsividade."
           id={uuidv4()}
         />
         <Box
-          title="React Blog"
-          subtitle="React.js | Firebase"
-          preview="Blog feito em react e firebase."
+          title="Clone Google Home"
+          subtitle="HTML5 | CSS3"
+          preview="Clone da home page do google."
           id={uuidv4()}
         />
         <Box
-          title="React Blog"
-          subtitle="React.js | Firebase"
-          preview="Blog feito em react e firebase."
+          title="Clone Google Search"
+          subtitle="HTML5 | CSS3"
+          preview="Clone da search page do google."
           id={uuidv4()}
         />
         <Box
-          title="React Blog"
-          subtitle="React.js | Firebase"
-          preview="Blog feito em react e firebase."
+          title="HDC Host"
+          subtitle="HTML5 | CSS3 | Responsividade"
+          preview="Institutional Page para serviços de hospedagem com responsividade."
           id={uuidv4()}
         />
         <Box
-          title="React Blog"
-          subtitle="React.js | Firebase"
-          preview="Blog feito em react e firebase."
+          title="Doceria"
+          subtitle="HTML5 | CSS3"
+          preview="Site de uma confeitaria com menu lateral."
           id={uuidv4()}
         />
         <Box
-          title="React Blog"
-          subtitle="React.js | Firebase"
-          preview="Blog feito em react e firebase."
+          title="Visit Card Page"
+          subtitle="HTML5 | CSS3"
+          preview="One Page com contato e formulário."
           id={uuidv4()}
         />
         <Box
-          title="React Blog"
-          subtitle="React.js | Firebase"
-          preview="Blog feito em react e firebase."
+          title="Institutional Page"
+          subtitle="HTML5 | CSS3"
+          preview="Página institucional genérica."
           id={uuidv4()}
         />
       </div>
