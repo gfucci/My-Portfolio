0f29701ec144d269a6d59421beb86c1dc0ff7861
Gabriel Fucci <gfucci_2005@hotmail.com>
2022-08-04 00:54:09 -0300

    Modifica HTML e CSS das competencias

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
   return (
     <div id='about_container'>
       <Title title='Gabriel Amaral Fucci' />
-      <p>Olá, sou um programador! Hoje o assunto que mais domino é programação web front-end, e estou estudando com foco em virar full stack. Além de TI também tenho conhecimentos na área de agrimensura e economia.</p>
+      <p>Olá, sou um programador! Hoje o assunto que mais domino é programação web front-end, e estou estudando com foco em virar full stack. Além de TI, sou formado em agrimensura e tenho conhecimentos de economia.</p>
       <h3>Contato</h3>
       <div id="contacts">
         <div id='contact1'>
@@ -65,102 +65,120 @@ const About = () => {
           </ul>
         </div>
       </div>
+      <div className="skills_container">
+        <div id="front_skills">
+          <h3>Habilidade Front-End</h3>
+          <ul>
+            <li>
+              <ProgressBar
+                technology="HTML"
+                percent={100}
+              />
+            </li>
+            <li>
+              <ProgressBar 
+                technology="React.js"
+                percent={85}
+              />
+            </li>
+            <li>
+              <ProgressBar
+                technology="CSS3"
+                percent={75}
+              />
+            </li>
+            <li>
+              <ProgressBar 
+                technology="JavaScript"
+                percent={70}
+              />
+            </li>
+            <li>
+              <ProgressBar 
+                technology="Redux"
+                percent={35}
+              />
+            </li>
+            <li>
+              <ProgressBar 
+                technology="TypeScript"
+                percent={35}
+              />
+            </li>
+          </ul>
+        </div>
+        <div id="back_skills">
+          <h3>Habilidade Back-End</h3>
+          <ul>
+            <li>
+              <ProgressBar
+                technology="Firebase"
+                percent={25}
+              />
+            </li>
+            <li>
+              <ProgressBar
+                technology="Node.js"
+                percent={25}
+              />
+            </li>
+            <li>
+              <ProgressBar
+                technology="Express.js"
+                percent={15}
+              />
+            </li>
+            <li>
+              <ProgressBar
+                technology="MongoDB"
+                percent={15}
+              />
+            </li>
+          </ul>
+        </div>
+        <div id="others_skills">
+          <h3>Outros</h3>
+          <ul>
+            <li>
+              <ProgressBar 
+                technology="Lógica de Programação"
+                percent={100}
+              />
+            </li>
+            <li>
+              <ProgressBar 
+                technology="Git / GitHub"
+                percent={90}
+              />
+            </li>
+            <li>
+              <ProgressBar 
+                technology="ReactNative"
+                percent={10}
+              />
+            </li>
+          </ul>
+        </div>
+        <div id="language_skills">
+          <h3>Idiomas</h3>
+          <ul>
+            <li>
+              <ProgressBar
+                technology="Português"
+                percent={100}
+              />
+            </li>
+            <li>
+              <ProgressBar
+                technology="Inglês"
+                percent={25}
+              />
+            </li>
+
+          </ul>
+        </div>
+      </div>
       {/*<h3>Competências</h3>
-      <ul>
-        <li>
-          <ProgressBar 
-            technology="Lógica de Programação"
-            percent={100}
-          />
-        </li>
-        <li>
-          <ProgressBar 
-            technology="HTML"
-            percent={100}
-          />
-        </li>
-        <li>
-          <ProgressBar 
-            technology="CSS3"
-            percent={75}
-          />
-        </li>
-        <li>
-          <ProgressBar 
-            technology="Git / GitHub"
-            percent={90}
-          />
-        </li>
-        <li>
-          <ProgressBar 
-            technology="JavaScript"
-            percent={70}
-          />
-        </li>
-        <li>
-          <ProgressBar 
-            technology="React.js"
-            percent={85}
-          />
-        </li>
-        <li>
-          <ProgressBar 
-            technology="Redux"
-            percent={35}
-          />
-        </li>
-        <li>
-          <ProgressBar 
-            technology="TypeScript"
-            percent={35}
-          />
-        </li>
-        <li>
-          <ProgressBar 
-            technology="Firebase"
-            percent={25}
-          />
-        </li>
-        <li>
-          <ProgressBar 
-            technology="Node.js"
-            percent={25}
-          />
-        </li>
-        <li>
-          <ProgressBar 
-            technology="Express.js"
-            percent={15}
-          />
-        </li>
-        <li>
-          <ProgressBar 
-            technology="MongoDB"
-            percent={15}
-          />
-        </li>
-        <li>
-          <ProgressBar 
-            technology="ReactNative"
-            percent={10}
-          />
-        </li>
-      </ul> 
-      <h3>Idiomas</h3>
-      <ul>
-        <li>
-          <ProgressBar
-            technology="Português"
-            percent={100}
-          />
-        </li>
-        <li>
-          <ProgressBar
-            technology="Inglês"
-            percent={25}
-          />
-        </li>
-      </ul>
       <h3>Links</h3>
       <button>
         <a href="https://github.com/gfucci/My-Curriculum" target="_blank" rel="noreferrer noopener">Código do Portfólio</a>
